0dba27f65f33ba38e0463f0d385c3463b8ee405d
henrique.castro <henrique.castro@pattrol.com.br>
2022-05-07 10:53:21 -0300

    implements minDistanceMatrix

--- Grafo.java
+++ Grafo.java
@@ -1,7 +1,7 @@
 public class Grafo {
     private int numEdges;
     private int numNodes;
-    private int edgesWeights[][];
+    public int edgesWeights[][];
 
     public Grafo(int numEdges, int numNodes) {
         this.numEdges = numEdges;
@@ -15,9 +15,26 @@ public class Grafo {
         }
     }
 
+    public int[][] getMinDistanceMatrix() {
+        int minPath[][] = this.edgesWeights.clone();
+
+        for (int k = 0; k < this.getNumNodes(); k++) {
+            for (int i = 0; i < this.getNumNodes(); i++) {
+                for (int j = 0; j < this.getNumNodes(); j++) {
+                    if(minPath[i][k] == Integer.MAX_VALUE || minPath[k][j] == Integer.MAX_VALUE)
+                        minPath[i][j] = minPath[i][j];
+                    else if (minPath[i][j] == Integer.MAX_VALUE)
+                        minPath[i][j] = minPath[i][k] + minPath[k][j];
+                    else if(minPath[i][j] > minPath[i][k] + minPath[k][j])
+                        minPath[i][j] = minPath[i][k] + minPath[k][j];
+                }
+            }
+        }
+        return minPath;
+    }
+
     public void setEdge0Indexed(int fromNode, int toNode, int weight) {
         this.edgesWeights[fromNode][toNode] = weight;
-        this.edgesWeights[toNode][fromNode] = weight;
     }
 
     public void setEdge(int fromNode, int toNode, int weight) {
--- Main.java
+++ Main.java
@@ -7,10 +7,12 @@ import java.util.Scanner;
  */
 public class Main {
     public static void main(String[] args) {
+        boolean isDirectional = true; 
         Grafo grafo = null;
         int pMedians = 0;
 
-        try (Scanner scanner = new Scanner(new File("instances/pmed01.txt"))) {
+        try (Scanner scanner = new Scanner(new File("instances/test.txt"))) {
+            int fromNode, toNode, weight;
             int numNodes = scanner.nextInt();
             int numEdges = scanner.nextInt();
             pMedians = scanner.nextInt();
@@ -18,15 +20,78 @@ public class Main {
             grafo = new Grafo(numEdges, numNodes);
 
             while(scanner.hasNextInt())
-                grafo.setEdge(scanner.nextInt(), scanner.nextInt(), scanner.nextInt());
+            {
+                fromNode = scanner.nextInt();
+                toNode = scanner.nextInt();
+                weight = scanner.nextInt();
+                grafo.setEdge(fromNode, toNode, weight);
+                if (!isDirectional)
+                    grafo.setEdge(toNode, fromNode, weight);
+            }
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        for (int i = 0; i < grafo.getNumNodes(); i++) {
-            int w = grafo.getEdgeWeight0Indexed(21, i);
-            if (w < Integer.MAX_VALUE)
-                System.out.println("fromNode: "+ i +", weight "+ w);
+
+        // for (int i = 0; i < 5; i++) {   
+        //     for (int j = 0; j < 5; j++) {
+        //         int num = grafo.getEdgeWeight0Indexed(i, j);
+        //         if (num == Integer.MAX_VALUE)
+        //             System.out.printf("inf\t");
+        //         else
+        //             System.out.printf("%d\t", num);
+        //     }
+        //     System.out.println("");
+        // }
+
+        int minDist[][] = grafo.getMinDistanceMatrix();
+        for (int i = 0; i < 5; i++) {   
+            for (int j = 0; j < 5; j++) {
+                System.out.printf("%d\t", minDist[i][j]);
+            }
+            System.out.println("");
         }
+
+        // for (int i = 0; i < grafo.getNumNodes(); i++) {
+        //     int w = grafo.getEdgeWeight0Indexed(21, i);
+        //     if (w < Integer.MAX_VALUE)
+        //         System.out.println("fromNode: "+ i +", weight "+ w);
+        // }
+        
+        // int minPath[][] = grafo.edgesWeights.clone();
+        // for (int k = 1; k <= grafo.getNumNodes(); k++) {
+        //     for (int i = 1; i <= grafo.getNumNodes(); i++) {
+        //         for (int j = 1; j <= grafo.getNumNodes(); j++) {
+        //             if(minPath[i][j] > minPath[i][k] + minPath[k][j])
+        //                 minPath[i][j] = minPath[i][k] + minPath[k][j];
+        //         }
+        //     }
+        // }
+        
+        // int minPath[][] = {
+        //     {0, 3, 8, Integer.MAX_VALUE, -4},
+        //     {Integer.MAX_VALUE, 0, Integer.MAX_VALUE, 1, 7},
+        //     {Integer.MAX_VALUE, 4, 0, Integer.MAX_VALUE, Integer.MAX_VALUE},
+        //     {2, Integer.MAX_VALUE, -5, 0, Integer.MAX_VALUE},
+        //     {Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE, 6, 0},
+        // };
+        // for (int k = 0; k < 5; k++) {
+        //     for (int i = 0; i < 5; i++) {
+        //         for (int j = 0; j < 5; j++) {
+        //             if(minPath[i][k] == Integer.MAX_VALUE || minPath[k][j] == Integer.MAX_VALUE)
+        //                 minPath[i][j] = minPath[i][j];
+        //             else if (minPath[i][j] == Integer.MAX_VALUE)
+        //                 minPath[i][j] = minPath[i][k] + minPath[k][j];
+        //             else if(minPath[i][j] > minPath[i][k] + minPath[k][j])
+        //                 minPath[i][j] = minPath[i][k] + minPath[k][j];
+        //         }
+        //     }
+        // }
+        // for (int i = 0; i < 5; i++) {   
+        //     for (int j = 0; j < 5; j++) {
+        //         System.out.printf("%d ", minPath[i][j]);
+        //     }
+        //     System.out.println("");
+        // }
     }
 }
