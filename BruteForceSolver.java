c5e9ec081fb84fe029f7ca4dc7bd468605149ff2
henrique.castro <henrique.castro@pattrol.com.br>
2022-05-09 11:22:16 -0300

    solving for k = 2

--- BruteForceSolver.java
+++ BruteForceSolver.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 
 public class BruteForceSolver {
@@ -13,45 +14,85 @@ public class BruteForceSolver {
         this.costMatrix = grafo.getMinDistanceMatrix();
     }
 
-    public int findBestCenter() {
-        int centerRadius, center = 0;
+    public ArrayList<Integer> findBestKCenter(int k) {
+        ArrayList<Integer> bestCenters = null, toNodes = null;
+        int minRadius = Integer.MAX_VALUE;
 
-        centerRadius = Integer.MAX_VALUE;
+        for (int i = 0; i < grafo.getNumNodes(); i++) {
+            for (int j = i+1; j < grafo.getNumNodes(); j++) {
 
-        for (int i = 0; i < this.grafo.getNumNodes(); i++) {
-            int maxDist = Integer.MIN_VALUE;
-            for (int j = 0; j < this.grafo.getNumNodes(); j++) {
-                if (maxDist < this.costMatrix[i][j])
-                    maxDist = this.costMatrix[i][j];
-            }
-            if (centerRadius > maxDist) {
-                center = i;
-                centerRadius = maxDist;
+                toNodes = new ArrayList<Integer>();
+                toNodes.add(i);
+                toNodes.add(j);
+
+                HashMap<Integer, ArrayList<Integer>> map = this.distributeNodesToCenters(toNodes);
+                map = this.getDistancesToCenters(map);
+
+                int tmp = this.findMaxRadiusOfDistribution(map);
+                if (minRadius > tmp)
+                {
+                    bestCenters = (ArrayList<Integer>) toNodes.clone();
+                    minRadius = tmp;
+                }
             }
         }
-        return center;
+
+        return bestCenters;
+    }
+
+    public int findMaxRadiusOfDistribution(HashMap<Integer, ArrayList<Integer>> distributedNodes)
+    {
+        Integer maxEntry = null;
+        int maxRadius = Integer.MIN_VALUE;
+
+        for (Integer entry : distributedNodes.keySet())
+        {
+            ArrayList<Integer> distances = distributedNodes.get(entry);
+            if (distances.size() > 0) {
+                int tmp = Collections.max(distances);
+                if (maxEntry == null || tmp > maxRadius)
+                {
+                    maxEntry = entry;
+                    maxRadius = tmp;
+                }
+            }
+        }
+        return maxRadius;
     }
 
     public HashMap<Integer, ArrayList<Integer>> distributeNodesToCenters(ArrayList<Integer> centers)
     {
         HashMap<Integer, ArrayList<Integer>> distributedNodes = new HashMap<Integer, ArrayList<Integer>>(centers.size());
 
-        for (int i = 0; i < centers.size(); i++) {
+        for (int i = 0; i < centers.size(); i++) 
             distributedNodes.put(centers.get(i), new ArrayList<Integer>());
-        }
 
         for (int i = 0; i < this.grafo.getNumNodes(); i++) {
-            int cheapestNode = this.getCheapestWithinNode(i, centers);
+            int cheapestNode = this.getCheapestWithinFromNode(i, centers);
             distributedNodes.get(cheapestNode).add(i);
         }
 
         return distributedNodes;
+    }    
+    
+    public HashMap<Integer, ArrayList<Integer>> getDistancesToCenters(HashMap<Integer, ArrayList<Integer>> distributedNodes)
+    {
+        HashMap<Integer, ArrayList<Integer>> distances = new HashMap<Integer, ArrayList<Integer>>(distributedNodes.size());
+        
+        for (Integer key : distributedNodes.keySet())
+        {
+            distances.put(key, new ArrayList<Integer>());
+            for (Integer node : distributedNodes.get(key))
+                distances.get(key).add(this.costMatrix[key][node]);
+        }
+
+        return distances;
     }
 
     /*
      * Returns the cheapest toNode, based on nodes passed as param
      */
-    public int getCheapestWithinNode(int fromNode, ArrayList<Integer> toNodes){
+    public int getCheapestWithinToNode(int fromNode, ArrayList<Integer> toNodes){
         int minDist, toNode;
 
         minDist = Integer.MAX_VALUE;
@@ -65,6 +106,25 @@ public class BruteForceSolver {
         }
         return toNode;
     }
+
+    /*
+     * Returns the cheapest FromNode, based on nodes passed as param
+     */
+    public int getCheapestWithinFromNode(int toNode, ArrayList<Integer> fromNodes){
+        int minDist, fromNode;
+
+        minDist = Integer.MAX_VALUE;
+        fromNode = fromNodes.get(0);
+
+        for (int i = 0; i < fromNodes.size(); i++) {
+            if(minDist > this.costMatrix[fromNodes.get(i)][toNode]) {
+                minDist =  this.costMatrix[fromNodes.get(i)][toNode];
+                fromNode = fromNodes.get(i);
+            }
+        }
+        return fromNode;
+    }
+
 
     int getCheapestNode(int fromNode) {
         int minDist, toNode;
