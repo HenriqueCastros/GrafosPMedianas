b40ab5cc14d36b28681982c04d50885715968376
Leonardo Gomide <leocgomide@gmail.com>
2022-05-11 19:58:30 -0300

    for all

--- Main.java
+++ Main.java
@@ -7,15 +7,20 @@ import java.util.Scanner;
  */
 public class Main {
     public static void main(String[] args) {
-        Grafo grafo = new Grafo("instances/pmed40.txt");
-        int kCenters = grafo.getKCenters();
+        Grafo grafo;
+        int kCenters;
+        for (int i = 1; i<=40; i++) {
+            grafo = new Grafo("instances/pmed"+i+".txt");
+            kCenters = grafo.getKCenters();
+        
+            MSTSolver mst = new MSTSolver(grafo, kCenters);
+            System.out.println("BestRadius '"+i+"': "+mst.findBestCenters().getValue());
+        }
         
         // int minDist[][] = grafo.getMinDistanceMatrix();
         
         // System.out.print(Grafo.graphMatrixToString(minDist));
         
-        MSTSolver mst = new MSTSolver(grafo, kCenters);
-        System.out.println(mst.findBestCenters());
         // int[][] subgraphs = mst.buildMSForest().getMinDistanceMatrix();
         
         // System.out.println(Grafo.graphMatrixToString(mst.buildMSForest().getMinDistanceMatrix()));
